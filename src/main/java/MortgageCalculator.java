89dcf33dd3027ef4827aa45a30f55b40d5cb9ac2
Michael Zhestkov <michaelzhestkov@gmail.com>
2016-03-28 20:26:11 -0700

    Update methods for MortgageCalculator.java

--- src/main/java/MortgageCalculator.java
+++ src/main/java/MortgageCalculator.java
@@ -10,41 +10,7 @@ public class MortgageCalculator {
 	static int termInMonths;
 	static String userName;
 
-	public static void anotherUser() {
-		System.out.println("Would you like to calculate data for another property? Yes or Not");
-		input = scanner.nextLine();
-	}
-
-	public static void calculateMonthlyPayment() {
-		double monthlyPayment = (principle * interest) / (1 - Math.pow(1 + interest, -termInMonths));
-		System.out.println("Your monthly payment is :" + monthlyPayment);
-
-	}
-
-	public static void fairwell() {
-		System.out.println(userName + ", thank you for using our application");
-	}
-
-	public static void main(String[] args) {
-
-		do {
-			welcome();
-			propertyId();
-			questions();
-			calculateMonthlyPayment();
-			anotherUser();
-		} while (input.equalsIgnoreCase("yes"));
-		fairwell();
-	}
-
-	public static void propertyId() {
-		System.out.println("What is your property ID?");
-		propertyId = scanner.nextLine();
-		System.out.println("Thanks, " + userName + ". Just to confirm, your property ID is " + propertyId);
-
-	}
-
-	public static void questions() {
+	public static void askQuestions() {
 		System.out.println("What is the principle? (ex: 200000)");
 		principle = Integer.parseInt(scanner.nextLine());
 		System.out.println("What is the interest? (ex: .0575)");
@@ -56,7 +22,41 @@ public class MortgageCalculator {
 
 	}
 
-	public static void welcome() {
+	public static void calculateMonthlyPayment() {
+		double monthlyPayment = (principle * interest) / (1 - Math.pow(1 + interest, -termInMonths));
+		System.out.println("Your monthly payment is :" + monthlyPayment);
+
+	}
+
+	public static void giveFairwell() {
+		System.out.println(userName + ", thank you for using our application");
+	}
+
+	public static void main(String[] args) {
+
+		do {
+			sayWelcome();
+			obtainPropertyId();
+			askQuestions();
+			calculateMonthlyPayment();
+			requestAnotherUser();
+		} while (input.equalsIgnoreCase("yes"));
+		giveFairwell();
+	}
+
+	public static void obtainPropertyId() {
+		System.out.println("What is your property ID?");
+		propertyId = scanner.nextLine();
+		System.out.println("Thanks, " + userName + ". Just to confirm, your property ID is " + propertyId);
+
+	}
+
+	public static void requestAnotherUser() {
+		System.out.println("Would you like to calculate data for another property? Yes or Not");
+		input = scanner.nextLine();
+	}
+
+	public static void sayWelcome() {
 		System.out.println("Please, enter your name?");
 		userName = scanner.nextLine();
 		System.out.println("Hello, " + userName + ". Welcome!");
